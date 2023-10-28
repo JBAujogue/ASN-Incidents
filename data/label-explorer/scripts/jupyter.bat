7eb3b847d80364043dda2b98f8bf9932f14a61da
Jean-baptiste Aujogue <jb.aujogue@gmail.com>
2023-10-28 21:59:44 +0200

    remove tmp file

--- data/label-explorer/scripts/jupyter.bat
+++
@@ -1,7 +0,0 @@
-@ECHO OFF
-call C:\Users\jbaujogue\Anaconda3\Scripts\activate.bat
-call conda activate cp_paperless
-cd %~dp0..
-jupyter notebook
-call conda deactivate
-pause
--- data/label-explorer/scripts/streamlit.bat
+++
@@ -1,7 +0,0 @@
-@ECHO OFF
-call C:\Users\jbaujogue\Anaconda3\Scripts\activate.bat
-call conda activate cp_paperless
-cd %~dp0..
-streamlit run streamlit\explorer.py --server.port 1177
-call conda deactivate
-pause
